ca97ffa85fd74468065b3bb8e96f402df73754ed
Jon Nolen <jon.nolen@gmail.com>
2013-02-03 13:02:16 -0500

    updating to render image correctly for ios user space.

--- DTiOSUtility/UIView+DTUtility.m
+++ DTiOSUtility/UIView+DTUtility.m
@@ -14,8 +14,15 @@
 - (UIImage *)viewSnapshot
 {
     UIGraphicsBeginImageContextWithOptions([self bounds].size, self.opaque, 0.0);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+
+    CGContextTranslateCTM(ctx, 0.0, CGRectGetHeight(self.bounds));
+    CGContextScaleCTM(ctx, 1.0, -1.0);
+
     [[self layer] renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    ctx = nil;
     UIGraphicsEndImageContext();
     
     return image;
